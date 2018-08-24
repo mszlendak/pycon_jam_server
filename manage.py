5d4bcf7711cffdbc29f53b7866b3404da330c1bf
mszlendak <rolus4414@gmail.com>
2018-08-24 02:21:25 +0200

    userinfo stub endpoint

--- manage.py
+++ manage.py
@@ -1,12 +1,26 @@
 from flask import Flask
+from flask_restplus import Api, Resource
 
 app = Flask(__name__)
+api = Api(app)
 
 
-@app.route('/')
-def index():
-    return 'Hello World'
-
+@api.route('/userinfo')
+class UserInfo(Resource):
+    def get(self):
+        return {
+	       'login': 'testStrig',
+	       'points': 213213,
+	       'stats': {
+		    'strength': 54,
+		    'dexterity': 54,
+		    'stamina': 54,
+		    'initiative': 47
+		    },
+		'items': list(),
+		'process_ogoing': list(),
+		'process_available': list()
+    }
 
 
 
